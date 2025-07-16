9c59d63b1fcc1976216704b2c5dcb4c8bc84ac86
xander.nesta <xandev.eth@protonmail.com>
2025-07-16 12:39:51 -0400

    update next.config.ts with webpack config to resolve pino-pretty missing error

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'out',
-  output: 'export',
-  images:{
+  distDir: "out",
+  output: "export",
+  images: {
     unoptimized: true,
   },
   basePath: "",
   assetPrefix: "./",
   trailingSlash: true,
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
